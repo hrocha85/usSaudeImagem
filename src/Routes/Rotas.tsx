e09f93fb32bf1d8a9230136cbc6109e69b7df821
ricardovalverde <valverde.ricardojr@gmail.com>
2022-09-21 13:14:49 -0300

    alteracoes

--- src/Routes/Rotas.tsx
+++ src/Routes/Rotas.tsx
@@ -1,15 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import AbdomemTotal from "../Main/exames/abdomemTotal";
-import Tireoide from "../Main/exames/tireoide";
-import SplashScreen from "../Main/splashScreen";
-import Configuracoes from "../Main/configuracao/configuracoes";
+import AbdomemTotal from "../main/abdomemTotal";
+import SplashScreen from "../main/splashScreen";
+import Configuracoes from "../main/configuracao/configuracoes";
 
 function Rotas() {
   return (
     <Routes>
       <Route path="/" element={<SplashScreen />} />
       <Route path="/AbdomemTotal" element={<AbdomemTotal />} />
-      <Route path="/Tireoide" element={<Tireoide />} />
       <Route path="/Configuracoes" element={<Configuracoes/>} />
     </Routes>
   );
