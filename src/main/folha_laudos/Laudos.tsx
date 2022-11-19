38944ec95034b0bfa551bd8b101b25a0ff58814d
ricardovalverde <valverde.ricardojr@gmail.com>
2022-11-19 16:58:58 -0300

    format pdf

--- src/main/folha_laudos/Laudos.tsx
+++ src/main/folha_laudos/Laudos.tsx
@@ -55,91 +55,96 @@ function Exames() {
 
   return (
     <>
-      
-        <Box className="zoom" boxShadow="xl" ref={ref}>
-          <Grid w="100%" gridTemplateRows={"15px 1fr 15px"}>
-            <Box margin="5px" display="flex" marginStart="15px">
-              <Image
-                src={clinicaSet.foto}
-                alt="Imagem Clínica"
-                boxSize="130px"
-                objectFit="scale-down"
-              />
-            </Box>
-
-            <Grid
-              templateColumns="repeat(1, 1fr)"
-              marginStart="50px"
-              justifyItems="center"
-              justifySelf="center"
-            >
-              <Text fontWeight="bold">{clinicaSet.nomeClinica}</Text>
-              <Text>{getPaciente()}</Text>
-              <Text>{getCurrentDate()}</Text>
-              <Text>{`Dr. ${medico.nome}`}</Text>
-            </Grid>
-          </Grid>
-          <Center>
-            <Divider
-              inlineSize="95%"
-              margin="5px"
-              borderColor="black"
-              marginTop="15px"
+      <Box className="zoom" boxShadow="xl" ref={ref}>
+        <Grid w="100%" gridTemplateRows={"15px 1fr 15px"}>
+          <Box margin="5px" display="flex" marginStart="15px">
+            <Image
+              src={clinicaSet.foto}
+              alt="Imagem Clínica"
+              boxSize="130px"
+              objectFit="scale-down"
             />
-          </Center>
-          <Box overflow="auto" h="35%" margin="20px">
-            <Text>{laudoPrin}</Text>
           </Box>
-          <Box position="absolute" bottom="5px" w="100%">
-            <HStack w="100%" justify="space-between">
-              <Grid templateColumns="repeat(1, 1fr)" justifyItems="center">
-                <Image
-                  src={medico.assinatura}
-                  alt="Assinatura Médico"
-                  boxSize="100px"
-                  backgroundImage="none"
-                />
-                <Divider
-                  inlineSize="30vh"
-                  margin="5px"
-                  marginTop="0px"
-                  borderColor="black"
-                />
 
-                <Text fontWeight="bold">{`Dr. ${medico.nome}`}</Text>
-                <Text fontWeight="bold">{`CRM ${medico.crm}`}</Text>
-              </Grid>
-
-              <Text
-                fontSize="11px"
-                fontStyle="italic"
-                fontWeight="bold"
-                overflowWrap="break-word"
-                paddingTop="12%"
-                paddingEnd="10px"
-              >
-                Santa Imagem Diagnósticos por imagem
-              </Text>
-            </HStack>
-            <Text fontSize="10" overflowWrap="break-word" margin="10px">
-              "A impressão diagnóstica em exames de imagem não é absoluta,
-              devendo ser correlacionada com dados clínicos, laboratorias e
-              outros métodos de imagem complementares"
-            </Text>
-          </Box>
-        </Box>
-        <Box>
-          <Link
-            display="block"
-            href={`#/Format_PDF`}
-            style={{ textDecoration: "none" }}
+          <Grid
+            templateColumns="repeat(1, 1fr)"
+            marginStart="50px"
+            justifyItems="center"
+            justifySelf="center"
           >
-            <Button colorScheme="blue" display="block" w="100%">
-              Entrar
-            </Button>
-          </Link>
+            <Text fontWeight="bold">{clinicaSet.nomeClinica}</Text>
+            <Text>{getPaciente()}</Text>
+            <Text>{getCurrentDate()}</Text>
+            <Text>{`Dr. ${medico.nome}`}</Text>
+          </Grid>
+        </Grid>
+        <Center>
+          <Divider
+            inlineSize="95%"
+            margin="5px"
+            borderColor="black"
+            marginTop="15px"
+          />
+        </Center>
+        <Box overflow="auto" h="35%" margin="20px">
+          <Text>{laudoPrin}</Text>
         </Box>
-      
+        <Box position="absolute" bottom="5px" w="100%">
+          <HStack w="100%" justify="space-between">
+            <Grid templateColumns="repeat(1, 1fr)" justifyItems="center">
+              <Image
+                src={medico.assinatura}
+                alt="Assinatura Médico"
+                boxSize="100px"
+                backgroundImage="none"
+              />
+              <Divider
+                inlineSize="30vh"
+                margin="5px"
+                marginTop="0px"
+                borderColor="black"
+              />
+
+              <Text fontWeight="bold">{`Dr. ${medico.nome}`}</Text>
+              <Text fontWeight="bold">{`CRM ${medico.crm}`}</Text>
+            </Grid>
+
+            <Text
+              fontSize="11px"
+              fontStyle="italic"
+              fontWeight="bold"
+              overflowWrap="break-word"
+              paddingTop="12%"
+              paddingEnd="10px"
+            >
+              Santa Imagem Diagnósticos por imagem
+            </Text>
+          </HStack>
+          <Text fontSize="10" overflowWrap="break-word" margin="10px">
+            "A impressão diagnóstica em exames de imagem não é absoluta, devendo
+            ser correlacionada com dados clínicos, laboratorias e outros métodos
+            de imagem complementares"
+          </Text>
+        </Box>
+      </Box>
+      <Box>
+        <Link
+          display="block"
+          href={`#/Format_PDF`}
+          style={{ textDecoration: "none" }}
+        >
+          <Button
+            colorScheme="blue"
+            right={1}
+            bottom={1}
+            position="fixed"
+            w='32%'
+            marginEnd='5px'
+          >
+            Gerar Laudo
+          </Button>
+        </Link>
+      </Box>
     </>
   );
 }
--- src/main/folha_laudos/format_pdf.tsx
+++ src/main/folha_laudos/format_pdf.tsx
@@ -1,31 +1,23 @@
-import { Flex, Text } from "@chakra-ui/react";
-import { pdf } from "@react-pdf/renderer";
-import {
-  useContext,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
-import { useNavigate } from "react-router-dom";
-import Pdf from "react-to-pdf";
+import React, { useContext, useState } from "react";
+import ReactPDF, {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Image,
+  PDFViewer,
+  Font,
+} from "@react-pdf/renderer";
 import { LaudosContext } from "../../context/LuadosContext";
+import { Center } from "@chakra-ui/react";
 
 export default function Format_PDF() {
-  const ref = useRef<HTMLDivElement | null>(null);
-
   const getUserClinica = () => {
     if (localStorage.getItem("user") != null) {
       var clinica = JSON.parse(localStorage.getItem("user")!);
     }
     return clinica.clinica;
-  };
-
-  const getUserMedico = () => {
-    if (localStorage.getItem("user") != null) {
-      var medico = JSON.parse(localStorage.getItem("user")!);
-    }
-    return medico.medico;
   };
 
   const getPaciente = () => {
@@ -35,9 +27,7 @@ export default function Format_PDF() {
       return "Nome paciente";
     }
   };
-  //
-  //
-  //
+
   const getCurrentDate = () => {
     const timeStamp = new Date();
 
@@ -45,49 +35,189 @@ export default function Format_PDF() {
       timeStamp.getMonth() + 1
     }/${timeStamp.getFullYear()}  ${timeStamp.getHours()}:${timeStamp.getMinutes()}:${timeStamp.getSeconds()}h`;
   };
-  const navigate = useNavigate();
+
+  const getUserMedico = () => {
+    if (localStorage.getItem("user") != null) {
+      var medico = JSON.parse(localStorage.getItem("user")!);
+    }
+    return medico.medico;
+  };
 
   const { laudoPrin } = useContext(LaudosContext);
   const [clinicaSet, setClinica] = useState<any>(JSON.parse(getUserClinica()));
   const [medico, setMedico] = useState(getUserMedico());
-  const [goBack, setGoback] = useState(false);
-  const [execute, setExecute] = useState(0);
 
-  useEffect(() => {
-    if (goBack) {
-      navigate(-1);
-      setExecute(0);
-    }
-  }, [goBack]);
+  Font.register({
+    family: "Montserrat",
+
+    fonts: [
+      {
+        src: "http://fonts.gstatic.com/s/montserrat/v25/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jqw16aX9-p7K5ILg.ttf",
+      },
+    ],
+  });
+  Font.register({
+    family: "Montserrat2",
+
+    fonts: [
+      {
+        src: "http://fonts.gstatic.com/s/montserrat/v25/JTUFjIg1_i6t8kCHKm459Wx7xQYXK0vOoz6jq5Z9aX9-p7K5ILg.ttf",
+      },
+    ],
+  });
+  Font.register({
+    family: "MontserratBold",
+
+    fonts: [
+      {
+        src: "http://fonts.gstatic.com/s/montserrat/v25/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCu170w-Y3tcoqK5.ttf",
+      },
+    ],
+  });
+
+  const styles = StyleSheet.create({
+    page: {
+      backgroundColor: "white",
+      color: "black",
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexDirection: "row",
+      width: "100%",
+    },
+    viewer: {
+      width: window.innerWidth, //the pdf viewer will take up all of the width and height
+      height: window.innerHeight,
+    },
+    imageClinica: {
+      width: 150,
+      height: 150,
+      objectFit: "scale-down",
+      alignContent: "center",
+    },
+    imageAssinatura: {
+      width: 100,
+      height: 100,
+      alignContent: "center",
+      justifyContent: "center",
+      alignSelf: "center",
+    },
+    viewAssinatura: {
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    sectionColuna: {
+      flexDirection: "column",
+      alignItems: "center",
+      marginTop: 30,
+      marginLeft: 80,
+    },
+
+    line: {
+      border: 1,
+      marginLeft: 10,
+      marginRight: 10,
+    },
+    laudo: {
+      margin: 30,
+      alignItems: "center",
+      alignSelf: "center",
+      justifyContent: "center",
+      flex: "1",
+      fontSize: 14,
+    },
+    pageNumber: {
+      position: "absolute",
+      bottom: 1,
+    },
+    footer: {
+      flexDirection: "row",
+      marginBottom: 20,
+      width: "100%",
+    },
+    footerColuna: {
+      flexDirection: "column",
+      margin: 20,
+    },
+    borderFooter: {
+      border: 1,
+      width: "30vh",
+    },
+    viewdadosMedico: {
+      alignItems: "center",
+      margin: 20,
+    },
+    textDadosMedico: {
+      fontFamily: "MontserratBold",
+      fontSize: 16,
+      color: "black",
+    },
+    textSantaImagem: {
+      marginTop: "19vh",
+      marginLeft: 50,
+      fontSize: "11",
+      fontFamily: "Montserrat",
+    },
+    textDiagnostico: {
+      margin: 10,
+      fontSize: "10",
+      fontFamily: "Montserrat2",
+    },
+  });
 
   return (
-    <>
-      <Pdf
-        targetRef={ref}
-        filename="code-example.pdf"
-        onComplete={() => setExecute(1)}
-      >
-        {({ toPdf }) => (
-          <Flex
-            ref={ref}
-            borderWidth="2px"
-            borderColor="black"
-            w="21cm"
-            h="29.7cm"
-            backgroundColor="grey"
-          >
-            <Text>sdgfsdfgsdfg</Text>
-            <>
-              {execute == 0
-                ? setTimeout(() => {
-                    toPdf();
-                    setExecute(1);
-                  }, 0)
-                : null}
-            </>
-          </Flex>
-        )}
-      </Pdf>
-    </>
+    <PDFViewer style={styles.viewer}>
+      <Document>
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section}>
+            <View style={styles.viewAssinatura}>
+              <Image style={styles.imageClinica} src={clinicaSet.foto} />
+            </View>
+
+            <View style={styles.sectionColuna}>
+              <Text>{clinicaSet.nomeClinica}</Text>
+              <Text>{getPaciente()}</Text>
+              <Text>{getCurrentDate()}</Text>
+              <Text>{`Dr. ${medico.nome}`}</Text>
+            </View>
+          </View>
+          <View style={styles.line}></View>
+          <View>
+            <Text style={styles.laudo}>{laudoPrin}</Text>
+          </View>
+          <View style={styles.pageNumber}>
+            <View style={styles.pageNumber}>
+              <View style={styles.footer}>
+                <View style={styles.footerColuna}>
+                  <Image
+                    style={styles.imageAssinatura}
+                    src={medico.assinatura}
+                  />
+                  <span style={styles.borderFooter}></span>
+                  <View style={styles.viewdadosMedico}>
+                    <Text
+                      style={styles.textDadosMedico}
+                    >{`Dr. ${medico.nome}`}</Text>
+                    <Text
+                      style={styles.textDadosMedico}
+                    >{`CRM ${medico.crm}`}</Text>
+                  </View>
+                </View>
+                <Text style={styles.textSantaImagem}>
+                  Santa Imagem Diagnósticos por imagem
+                </Text>
+              </View>
+            </View>
+
+            <Text style={styles.textDiagnostico}>
+              "A impressão diagnóstica em exames de imagem não é absoluta,
+              devendo ser correlacionada com dados clínicos, laboratorias e
+              outros métodos de imagem complementares"
+            </Text>
+          </View>
+        </Page>
+      </Document>
+    </PDFViewer>
   );
 }
