db4e1c1ac6f6048b53b0c527a8bfca2c99bf9194
ricardovalverde <valverde.ricardojr@gmail.com>
2023-02-28 23:57:14 -0300

    implant funcao format laudo para remover conclusao

--- src/main/component/function_format_laudo.ts
+++ src/main/component/function_format_laudo.ts
@@ -1,15 +1,15 @@
 export class Format_Laudo {
-  titulo_exame: string;
-  sub_exame_nome: string;
-  frases_is_empty: boolean;
-  frases_sub_exame: Array<string>;
+  titulo_exame?: string;
+  sub_exame_nome?: string;
+  frases_is_empty?: boolean;
+  frases_sub_exame?: Array<string>;
   conclusoes_sub_exame?: Array<string>;
 
   constructor(
-    titulo_exame: string,
-    sub_exame_nome: string,
-    frases_is_empty: boolean,
-    frases_sub_exame: Array<string>,
+    titulo_exame?: string,
+    sub_exame_nome?: string,
+    frases_is_empty?: boolean,
+    frases_sub_exame?: Array<string>,
     conclusoes_sub_exame?: Array<string>
   ) {
     this.titulo_exame = titulo_exame;
@@ -32,11 +32,11 @@ export class Format_Laudo {
               subExame.subExameNome = null;
             }
           });
-          localStorage.removeItem(this.sub_exame_nome);
+          localStorage.removeItem(this.sub_exame_nome!);
         } else {
           if (
-            localStorage.getItem(this.sub_exame_nome) == "0" ||
-            localStorage.getItem(this.sub_exame_nome) == null
+            localStorage.getItem(this.sub_exame_nome!) == "0" ||
+            localStorage.getItem(this.sub_exame_nome!) == null
           ) {
             Exames.subExames.push({
               subExameNome: this.sub_exame_nome,
@@ -50,13 +50,13 @@ export class Format_Laudo {
                 Exames.conclusoes.push(conclusao);
               });
             }
-            localStorage.setItem(this.sub_exame_nome, JSON.stringify(1));
+            localStorage.setItem(this.sub_exame_nome!, JSON.stringify(1));
           }
         }
         Exames.subExames.map((subExame, indexS) => {
           if (subExame.subExameNome == this.sub_exame_nome) {
             Exames.subExames[indexS].frases = this.frases_sub_exame;
-            localStorage.setItem(this.sub_exame_nome, JSON.stringify(1));
+            localStorage.setItem(this.sub_exame_nome!, JSON.stringify(1));
           }
         });
       }
@@ -64,5 +64,20 @@ export class Format_Laudo {
     localStorage.setItem("format_laudo", JSON.stringify(array));
     window.localStorage.setItem("isThisInLocalStorage", JSON.stringify(1));
     window.dispatchEvent(new Event("storage"));
+  }
+
+  Remove_Conclusao(conclusao): void {
+    var array = JSON.parse(localStorage.getItem("format_laudo")!);
+
+    array.map((Exames) => {
+      if (Exames.titulo_exame == this.titulo_exame) {
+        Exames.conclusoes.map((e, index) => {
+          if (e === conclusao) {
+            Exames.conclusoes.splice(index, 1);
+            localStorage.setItem("format_laudo", JSON.stringify(array));
+          }
+        });
+      }
+    });
   }
 }
