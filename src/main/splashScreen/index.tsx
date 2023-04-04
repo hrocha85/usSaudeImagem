8b3c2bfd45b9e6c9fa4225083d60c16f9970f19b
ricardovalverde <valverde.ricardojr@gmail.com>
2023-04-04 11:44:41 -0300

    fix localstorage.clear

--- src/main/splashScreen/index.tsx
+++ src/main/splashScreen/index.tsx
@@ -1,25 +1,31 @@
 import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
-import logo from './logo.png'
-import './styles.css'
+import logo from "./logo.png";
+import "./styles.css";
 
 const SplashScreen = () => {
-  localStorage.clear()
+  const keysToKeep = ["chakra-ui-color-mode"];
+
+  keysToKeep.forEach((key) => {
+    localStorage.clear();
+    localStorage.setItem(key, "light");
+  });
 
   const [redirectNow, setRedirectNow] = useState(false);
   //tempo de 5 segundos para sair da pagina
   setTimeout(() => setRedirectNow(true), 5000);
   //está sendo jogado para abdomen total pqe nao temos a pagina principal, ajustar isso futuramente
-  return redirectNow ? (<Navigate to="/Login" />) :
-    (
-      <div className='body'>
-        <div className='container'>
-          <div className='box'>
-            <img src={logo} alt="logo" />
-          </div>
+  return redirectNow ? (
+    <Navigate to="/Login" />
+  ) : (
+    <div className="body">
+      <div className="container">
+        <div className="box">
+          <img src={logo} alt="logo" />
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default SplashScreen;
