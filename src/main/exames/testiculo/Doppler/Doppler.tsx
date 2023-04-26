a1a79ee73a9284f28e47f447729981d1a6e16445
LucasBGarcia <barbozagarcia@yahoo.com.br>
2023-04-26 14:11:04 -0300

    finaliza correções de testiculo

---
+++ src/main/exames/testiculo/Doppler/Doppler.tsx
@@ -0,0 +1,180 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable react-hooks/exhaustive-deps */
+import {
+  Box,
+  Checkbox,
+  Flex,
+  HStack,
+  Input,
+  Radio,
+  RadioGroup,
+  Stack,
+  Text
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Format_Laudo } from "../../../component/function_format_laudo";
+import TituloNomeExame from "../../../component/titulo_nome_exame";
+
+function Doppler() {
+  const altura = "100%";
+  const largura = "66%";
+
+  const [FrasesDoppler, setFrasesDoppler] = useState<any>(
+    []
+  );
+  const [DescreverDoppler, setDescreverDoppler] = useState(false);
+
+  const [Inflamacao, setInflamacao] = useState(false);
+  const [NaoHaSinaisCheckbox, setNaoHaSinaisCheckbox] = useState(false);
+  const [Dilatacao, setDilatacao] = useState(false);
+  const [DilatacaoInput1, setDilatacaoInput1] = useState('');
+  const [CalibreInput1, setCalibreInput1] = useState('');
+
+  useEffect(() => {
+    const string = 'Não há sinais de varizes a manobra de Valsalva no estudo doppler colorido.'
+    NaoHaSinaisCheckbox ? setFrasesDoppler((arr) => [...arr, string]) : removeItemString(string)
+  }, [NaoHaSinaisCheckbox])
+  useEffect(() => {
+    const string = 'Não sabemos a frase.'
+    Inflamacao ? setFrasesDoppler((arr) => [...arr, string]) : removeItemString(string)
+  }, [Inflamacao])
+
+  const criaStringDilatacao = () => {
+    var string = 'Dilatação e discreta tortuosidade de vasos do plexo pampiniforme, medindo no repouso'
+    removeSelect(string)
+    if (Dilatacao) {
+      if (DilatacaoInput1 && CalibreInput1) {
+        string = `${string} ${DilatacaoInput1} cm (normal <0.2-0,3cm) e acentuando-se com as manobras de Valsalva, alcançando calibre de ${CalibreInput1} cm, associado a refluxo valvar.`
+        setFrasesDoppler((arr) => [...arr, string])
+      }
+    } else {
+      setDilatacaoInput1('')
+      setCalibreInput1('')
+    }
+  }
+
+  useEffect(() => {
+    criaStringDilatacao()
+  }, [Dilatacao, CalibreInput1, DilatacaoInput1])
+
+  const removeSelect = (value) => {
+    FrasesDoppler.map((e) => {
+      if (e.includes(value)) {
+        var index = FrasesDoppler.indexOf(e);
+
+        if (index > -1) {
+          FrasesDoppler.splice(index, 1);
+          setFrasesDoppler((arr) => [...arr]);
+        }
+      }
+    });
+  };
+
+  const removeItemString = (value) => {
+    FrasesDoppler.map((e) => {
+      if (e.includes(value)) {
+        var index = FrasesDoppler.indexOf(e);
+
+        if (index > -1) {
+          FrasesDoppler.splice(index, 1);
+          setFrasesDoppler((arr) => [...arr]);
+        }
+      }
+    });
+  };
+  useEffect(() => {
+    if (DescreverDoppler) {
+      new Format_Laudo().Change_Title("Testículo", titulo_exame, true);
+    } else {
+      new Format_Laudo().Change_Title("Testículo", titulo_exame, false);
+    }
+  }, [DescreverDoppler]);
+
+  const subExame = "Doppler";
+  var titulo_exame = "Testículo com doppler";
+
+  useEffect(() => {
+    if (Object.keys(FrasesDoppler).length == 0) {
+      new Format_Laudo(
+        titulo_exame,
+        subExame,
+        true,
+        FrasesDoppler
+      ).Format_Laudo_Create_Storage();
+    } else {
+      new Format_Laudo(
+        titulo_exame,
+        subExame,
+        false,
+        FrasesDoppler
+      ).Format_Laudo_Create_Storage();
+    }
+  }, [FrasesDoppler]);
+
+
+  return (
+    <Box
+      bg="#FAFAFA"
+      w={largura}
+      h={altura}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      borderRadius="10.85px"
+      boxShadow="md"
+      padding="24px 15px 10px 15px"
+      mt="20px"
+    >
+      <Checkbox onChange={() => setDescreverDoppler(!DescreverDoppler)}>
+        <TituloNomeExame titulo="Descrever Doppler" />
+      </Checkbox>
+      <Box display='flex' flexWrap='wrap' gap='10px'>
+
+        <Checkbox
+          isDisabled={!DescreverDoppler || Dilatacao}
+          onChange={() => setNaoHaSinaisCheckbox(!NaoHaSinaisCheckbox)}
+        >
+          Não há sinais de varizes
+        </Checkbox>
+
+        <HStack>
+          <Checkbox
+            isDisabled={!DescreverDoppler || NaoHaSinaisCheckbox}
+            onChange={() => setDilatacao(!Dilatacao)}
+          >
+            Dilatação e discreta tortuosidade de vasos do plexo
+          </Checkbox>
+          <Text>Medida repouso</Text>
+          <Input
+            isDisabled={!Dilatacao}
+            value={DilatacaoInput1}
+            w='60px'
+            padding="0px"
+            textAlign="center"
+            onChange={(e) => setDilatacaoInput1(e.target.value)}
+            placeholder={"cm"}
+          />
+          <Text>Calibre</Text>
+          <Input
+            isDisabled={!Dilatacao}
+            value={CalibreInput1}
+            w='60px'
+            padding="0px"
+            textAlign="center"
+            onChange={(e) => setCalibreInput1(e.target.value)}
+            placeholder={"cm"}
+          />
+
+        </HStack>
+        <Checkbox
+          isDisabled={!DescreverDoppler}
+          onChange={() => setInflamacao(!Inflamacao)}
+        >
+          Inflamação
+        </Checkbox>
+      </Box>
+
+    </Box >
+  );
+}
+
+export default Doppler;
--- src/main/exames/testiculo/index.tsx
+++ src/main/exames/testiculo/index.tsx
@@ -7,8 +7,41 @@ import Hidrocele from "./hidrocele/hidrocele";
 import Testiculos from "./medidas/medidas";
 import Microlitiase from "./microlitiase/microlitiase";
 import NodulosTesticulares from "./nodulosTesticulares/nodulosTesticulares";
+import { useEffect, useState } from "react";
+import { Format_Laudo } from "../../component/function_format_laudo";
+import Doppler from "./Doppler/Doppler";
 
 function Testiculo() {
+
+  const stringPadrao2 =
+    "Exame realizado em modo bidimensional, com equipamento dinâmico linear multifrequêncial. Foram feitas varreduras nos sentidos transversal, longitudinal e oblíquos.";
+
+  const [frasesMedidas, setFrasesMedidas] = useState<any>([stringPadrao2]);
+
+  // useEffect(()=>{
+  // setFrasesMedidas()
+  // },[])
+  const subExame = "testículos";
+  const titulo_exame = "Testículo";
+
+
+  useEffect(() => {
+    if (Object.keys(frasesMedidas).length == 0) {
+      new Format_Laudo(
+        titulo_exame,
+        subExame,
+        true,
+        frasesMedidas,
+      ).Format_Laudo_Create_Storage();
+    } else {
+      new Format_Laudo(
+        titulo_exame,
+        subExame,
+        false,
+        frasesMedidas,
+      ).Format_Laudo_Create_Storage();
+    }
+  }, [frasesMedidas]);
   return (
     <>
       <Box ml="10px" paddingBottom="1%">
@@ -29,6 +62,7 @@ function Testiculo() {
           {/*<Virococele />*/}
         </Grid>
         <Hidrocele />
+        <Doppler />
       </Box>
     </>
   );
