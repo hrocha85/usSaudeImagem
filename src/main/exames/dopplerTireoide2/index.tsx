b4a2081eb3c9e704ac8cd0eafd9b5ef868467b90
LucasBGarcia <barbozagarcia@yahoo.com.br>
2023-08-15 14:41:58 -0300

    fix Errors eslint

--- src/main/exames/dopplerTireoide2/index.tsx
+++ src/main/exames/dopplerTireoide2/index.tsx
@@ -1,11 +1,10 @@
 import { Box } from "@chakra-ui/react";
 
 import Cirurgias from "./cirurgias/cirurgias";
-import Cisto from "./cisto/cisto";
+
 import EcotexturaParenquima from "./EcotexturaParenquima/EcotexturaParenquima";
 import Idade from "./idade/idade";
 import Medidas from "./medidas/medidas";
-import Nodulos from "./nodulos/nodulos";
 
 function DopplerTireoide2() {
   return (
@@ -20,12 +19,8 @@ function DopplerTireoide2() {
         <EcotexturaParenquima></EcotexturaParenquima>
 
         <Cirurgias></Cirurgias>
-
-        <Cisto></Cisto>
-
         <Idade></Idade>
 
-        <Nodulos></Nodulos>
       </Box>
     </>
   );
