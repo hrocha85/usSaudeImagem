f513921878a9e7c4e3e0d3561896308727aacbf2
Taiwan marinho <clownser1994@gmail.com>
2023-08-21 11:11:10 -0300

    feat:Melhorias e responsividade na tela de exames Prostata

--- src/main/exames/prostata/impressao_diagnostica/impressao_diagnostica.tsx
+++ src/main/exames/prostata/impressao_diagnostica/impressao_diagnostica.tsx
@@ -1,11 +1,13 @@
-import { Checkbox, Flex } from "@chakra-ui/react";
+import { Checkbox, Flex, useMediaQuery } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { Format_Laudo } from "../../../component/function_format_laudo";
 import TituloNomeExame from "../../../component/titulo_nome_exame";
 
 function ImpressaoDiagnostica() {
   const altura = "100%";
-  const largura = "45%";
+  let largura = "60%";
+  const [isLargerThan600] = useMediaQuery('(min-width: 600px)')
+  isLargerThan600 ? largura = "60%": largura = "100%"
 
   const [frasesProstata, setFrasesProstata] = useState<any>([]);
 
